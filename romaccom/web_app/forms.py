9ed6da21bf799106d4efe1a884307fc339081c10
João Manuel <2958530K@student.gla.ac.uk>
2025-03-11 23:18:28 +0000

    Add review form

--- romaccom/web_app/forms.py
+++ romaccom/web_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
-from .models import UserProfile
+from .models import UserProfile, Review
 
 User = get_user_model()
 
@@ -18,3 +18,11 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['website', 'picture']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['title', 'rating', 'review_text']
+        widgets = {
+            'review_text': forms.Textarea(attrs={'rows': 4}),
+        }

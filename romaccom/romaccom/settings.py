612cdfd150c2ef464c5381ef858ee8682fc96c8b
Yike Liu <2904316l@student.gla.ac.uk>
2025-03-13 02:48:54 +0000

    added static root

--- romaccom/romaccom/settings.py
+++ romaccom/romaccom/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

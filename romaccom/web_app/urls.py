b0008fa11b9423b1456f5ae6b77e8c2e6cc0fa4c
Illiauofg <2833295b@student.gla.ac.uk>
2025-03-13 20:54:01 +0000

    Added feature to allow user to delete account

--- romaccom/web_app/urls.py
+++ romaccom/web_app/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('operator/dashboard/addnewaccommodation/', views.add_accommodation_view, name='add_accommodation'),
     path('operator/dashboard/manageaccommodationinfo/', views.manage_accom_info_view, name='manage_accom_info'),
     path('user/myaccount/update-privacy/', views.update_privacy_view, name='update-privacy'),
+    path('user/myaccount/delete/', views.delete_account, name='delete_account'),
     path('operator/management/', views.management_view, name='management'),
 
     path('api/accommodation/upload-images/', views.upload_accommodation_images_view, name='upload_accommodation_images'),

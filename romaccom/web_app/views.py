bf55d0afde30cb02a5198ffe43fc634b3f53dbd9
Yike Liu <2904316l@student.gla.ac.uk>
2025-03-15 06:38:52 +0000

    user now will be taken to myaccount page after login, not home page

--- romaccom/web_app/views.py
+++ romaccom/web_app/views.py
@@ -140,7 +140,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('home')
+            return redirect('myaccount')
         else:
             return render(request, 'romaccom/login.html', {'error': 'Invalid username or password'})
     return render(request, 'romaccom/login.html')
